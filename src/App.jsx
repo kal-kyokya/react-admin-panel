d643c7dfd936af78c7340223caefff4a5ee681a2
kal-kyokya <kalkyokya4@gmail.com>
2025-03-19 09:27:30 +0000

    Design the 'insights' sidebar option and ready addition of a chart component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import './app.scss';
 import Topbar from './components/topbar/Topbar';
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './pages/home/Home';
+import Insights from './pages/insights/Insights';
 
 function App() {
     return (<div>
 		<Topbar />
 		<div className='container'>
 		    <Sidebar />
-		    <Home />
+		    <Insights />
 		</div>
 	    </div>);
 }
---
+++ src/pages/insights/Insights.jsx
@@ -0,0 +1,12 @@
+import './insights.scss';
+import Chart from '../../components/chart/Chart';
+
+const Insights = () => {
+    return (
+	<div className='insights'>
+	    <Chart />
+	</div>
+    );
+}
+
+export default Insights;
