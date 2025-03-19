03adaa7379ee5968e9a69feb128121c1d544f574
kal-kyokya <kalkyokya4@gmail.com>
2025-03-19 08:48:09 +0000

    Complete design of the 'home' sidebar option.

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,36 @@
+import './widget.scss';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+
+const Widget = () => {
+    return (
+	<div className='widget'>
+	    <div className='widgetItems'>
+		<span className='widgetTitle'>Users</span>
+		<div className='widgetData'>
+		    <span className='widgetDataFigure'>+39 | </span>
+		    <span className='widgetDataPercent'>52%<ArrowUpwardIcon className='widgetIcons' /></span>
+		</div>
+		<span className='widgetText'>Compared to last month.</span>
+	    </div>
+	    <div className='widgetItems'>
+		<span className='widgetTitle'>Revenue</span>
+		<div className='widgetData'>
+		    <span className='widgetDataFigure'>+91,000Ksh | </span>
+		    <span className='widgetDataPercent'>78%<ArrowUpwardIcon className='widgetIcons' /></span>
+		</div>
+		<span className='widgetText'>Compared to last month.</span>
+	    </div>
+	    <div className='widgetItems'>
+		<span className='widgetTitle'>Uploads</span>
+		<div className='widgetData'>
+		    <span className='widgetDataFigure'>+13 | </span>
+		    <span className='widgetDataPercent'>26%<ArrowDownwardIcon className='widgetIcons negative' /></span>
+		</div>
+		<span className='widgetText'>Compared to last month.</span>
+	    </div>
+	</div>
+    );
+}
+
+export default Widget;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import './home.scss';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import Widget from '../../components/widget/Widget';
+
+const Home = () => {
+    return (
+	<div className='home'>
+	    <Widget />
+	</div>
+    );
+}
+
+export default Home;
