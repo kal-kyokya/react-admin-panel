3e2327441b0de7cc2166168d3e2582fc344a2b39
kal-kyokya <kalkyokya4@gmail.com>
2025-03-20 12:16:31 +0000

    Implement 'delete' feature on 'UserList'.

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -4,52 +4,61 @@ import Paper from '@mui/material/Paper';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import { rows } from '../../dummyData';
 import { Link } from 'react-router-dom';
-
-const columns = [
-  { field: 'id', headerName: 'ID', width: 91 },
-  { field: 'username', headerName: 'Username', width: 143, renderCell: (params) => {
-      return (
-	  <div className='userListCell'>
-	      <img className='profile' src={ params.row.picture } alt='Profile Pic'/>
-	      { params.row.username }
-	  </div>
-      );}
-  },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age', headerName: 'Age', width: 90,
-    type: 'number',
-  },
-  {
-    field: 'fullName', headerName: 'Full name', width: 160, sortable: false,
-    description: 'This column has a value getter and is not sortable.',
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
-    {
-	field: 'manage', headerName: 'Manage', width: 91, renderCell: (params) => {
-	    return (
-		<div className='manageUser'>
-		    <Link to={ '/user/' + params.row.id }
-			  className='link'>
-			<button className='manageUserButton'>Edit</button>
-		    </Link>
-		    <DeleteOutlineIcon className='deleteIcon' />
-		</div>
-	    );
-	}
-    },
-];
-
-const paginationModel = { page: 0, pageSize: 10 };
+import { useState } from 'react';
 
 const UserList = () => {
+    const [userData, setUserData] = useState(rows);
+
+    const handleDelete = (id) => {
+	setUserData(userData.filter((user) => user.id !== id));
+    };
+
+    const columns = [
+	{ field: 'id', headerName: 'ID', width: 91 },
+	{ field: 'username', headerName: 'Username', width: 143, renderCell: (params) => {
+	    return (
+		<div className='userListCell'>
+		    <img className='profile' src={ params.row.picture } alt='Profile Pic'/>
+		    { params.row.username }
+		</div>
+	    );}
+	},
+	{ field: 'firstName', headerName: 'First name', width: 130 },
+	{ field: 'lastName', headerName: 'Last name', width: 130 },
+	{
+	    field: 'age', headerName: 'Age', width: 90,
+	    type: 'number',
+	},
+	{
+	    field: 'fullName', headerName: 'Full name', width: 160, sortable: false,
+	    description: 'This column has a value getter and is not sortable.',
+	    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+	},
+	{
+	    field: 'manage', headerName: 'Manage', width: 91, renderCell: (params) => {
+		return (
+		    <div className='manageUser'>
+			<Link to={ '/user/' + params.row.id }
+			      className='link'>
+			    <button className='manageUserButton'>Edit</button>
+			</Link>
+			<DeleteOutlineIcon className='deleteIcon'
+					   onClick={ () => handleDelete(params.row.id) }/>
+		    </div>
+		);
+	    }
+	},
+    ];
+
+    const paginationModel = { page: 0, pageSize: 10 };
+
+
     return (
 	<div className='userList'>
 	    <Paper sx={{ height: '100%', width: '100%' }}>
 		<DataGrid
-		    rows={rows}
-		    columns={columns}
+		    rows={ userData }
+		    columns={ columns }
 		    disableRowSelectionOnClick
 		    checkboxSelection		    
 		    initialState={{ pagination: { paginationModel } }}
